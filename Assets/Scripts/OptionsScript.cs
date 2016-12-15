48626e6236e00ef9838469898ee115156ab223bd
dewisekar <sekar.6499@gmail.com>
2016-12-16 03:53:18 +0700

    Menu+option dewi
    masih on progress

---
+++ Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionsScript : MonoBehaviour {
+
+public void ChangeToOptions (string sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+}
