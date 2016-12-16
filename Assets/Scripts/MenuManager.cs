2a7dd1072426e66f99e835fbb73baeb6bf91e2f7
dewisekar <sekar.6499@gmail.com>
2016-12-16 12:19:16 +0700

    Update menu dewi
    kurang kontrol button sama sound cuma masih bisa di satu scene

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject mainMenuHolder;
+	public GameObject optionsMenuHolder;
+
+	public Slider[] volumesliders;
+
+	public void play(){
+		SceneManager.LoadScene ("main");
+	}
+
+	public void exit(){
+		Application.Quit ();
+	}
+
+	public void option(){
+		SceneManager.LoadScene ("Option scene dewi");
+	}
+
+	public void ChangeToOptions (string sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+
+	public void setMasterVolume (float value){
+		
+	}
+}
--- Assets/Scripts/OptionsScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class OptionsScript : MonoBehaviour {
-
-public void ChangeToOptions (string sceneToChangeTo) {
-		Application.LoadLevel (sceneToChangeTo);
-	}
-}
