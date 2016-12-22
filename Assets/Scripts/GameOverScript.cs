16213661667e41929058d39ea081d1e3c92d7473
uvertime <chrisnady99@gmail.com>
2016-12-22 20:03:11 +0700

    Game Over Text
    Text di Game Over

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class GameOverScript : MonoBehaviour {
 	public GUIText ScoreFinal;
-	
+	public GUIText WaveFinal;
 	void Awake(){
-		ScoreFinal.text = "Score: " + GameController.score;
+		ScoreFinal.text = "Your Score: " + GameController.score;
+		WaveFinal.text = "Your last wave is " + GameController.wavet;
 	}
 }
