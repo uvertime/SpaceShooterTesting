fc595b4a34a40bae52d90af69596a5fba89dd51f
dewisekar <sekar.6499@gmail.com>
2016-12-23 11:17:55 +0700

    Yay jadi
    tinggal deploy

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -33,4 +33,8 @@ public class MenuManager : MonoBehaviour {
 	public void storyscene(){
 		SceneManager.LoadScene ("Story");
 	}
+
+	public void tocreds(){
+		SceneManager.LoadScene ("Credits");
+	}
 }
